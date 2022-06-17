624015c7821294624e3438a845dddd6e3ec3fc26
xRate1337 <root@lizenztest>
2022-06-17 17:59:02 +0200

    lol

---
+++ src/main/java/Vivavis/App.java
@@ -0,0 +1,13 @@
+package Vivavis;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
